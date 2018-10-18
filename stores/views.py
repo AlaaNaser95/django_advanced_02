028d487bf69b669a2fd8c6ab27cda21933a7d85d
AlaaNaser95 <alaanaser95.95@gmail.com>
2018-10-18 13:44:30 +0300

    task submission

--- stores/views.py
+++ stores/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Store
 from .forms import StoreModelForm
+from django.contrib import messages
 
 # Create your views here.
 def store_list(request):
@@ -10,4 +11,23 @@ def store_list(request):
     return render(request, 'store_list.html', context)
 
 
+def create_view(request):
 
+    form = StoreModelForm()
+    if request.method == "POST":
+        form = StoreModelForm(request.POST, request.FILES or None)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Successfully Created!")
+            return redirect('list')
+    context = {
+    "form": form,
+    }
+    return render(request, 'create.html', context)
+
+def store_detail(request,store_slug):
+    store = Store.objects.get(slug=store_slug)
+    context = {
+        "store": store,
+    }
+    return render(request, 'detail.html', context)
